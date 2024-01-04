0c51a591544798558a2516b7ce6797a1f300331d
LautareteX <lautarorodriguezcoitinho@gmail.com>
2024-01-04 14:59:17 -0800

    Task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" type-annotated function sum_list"""
+
+
+def sum_list(input_list: list) -> float:
+    """ type-annotated function sum_list"""
+    return sum(input_list)
