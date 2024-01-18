78aeeb2cd620f3de9d59706b44c401781069fec7
LautareteX <lautarorodriguezcoitinho@gmail.com>
2024-01-18 02:07:56 -0800

    task 0

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,15 +1,14 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.'; //const can never be modified
+    const task = 'I prefer const when I can.';  //can never be modified
     return task;
 }
-  
+
 export function getLast() {
     return ' is okay';
 }
-  
+
 export function taskNext() {
-    let combination = 'But sometimes let'; //can be modified outside this block
+    let combination = 'But sometimes let';  //can be modified outside this block
     combination += getLast();
-  
     return combination;
 }
