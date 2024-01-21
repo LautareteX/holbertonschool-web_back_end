0b81adb260ca684daab4ef5dd263880137631a8d
LautareteX <lautarorodriguezcoitinho@gmail.com>
2024-01-21 15:59:19 -0800

    task 2 - rewrite

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,25 +1,25 @@
 export default class HolbertonCourse {
-constructor(name = '', length = 0, students = []) {
+  constructor(name = '', length = 0, students = []) {
     this.name = name;
-        this.length = length;
-        this.students = students;
+    this.length = length;
+    this.students = students;
     }
-      set name(Name) {
-        if (typeof Name !== 'string') {
-            throw TypeError('Name must be a string');
-        }
-        this._name = Name;
+  set name(Name) {
+    if (typeof Name !== 'string') {
+      throw TypeError('Name must be a string');
     }
-    set length(Length) {
-        if (typeof Length !== 'number') {
-            throw TypeError('Length must be a number');
-        }
-        this._length = Length;
+    this._name = Name;
     }
-    set students(Students) {
-        if (Array.isArray(Students)
+  set length(Length) {
+    if (typeof Length !== 'number') {
+      throw TypeError('Length must be a number');
+     }
+     this._length = Length;
+    }
+  set students(Students) {
+    if (Array.isArray(Students)
             && Students.every((student) => (typeof student === 'string') === true)) {
-            this._students = Students;
+     this._students = Students;
         } else {
             throw TypeError('Students must be an array of strings');
         }
