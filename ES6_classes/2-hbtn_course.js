4c248aa0e68d2ac8cf6cfcd8c730f044b9c96ca7
LautareteX <lautarorodriguezcoitinho@gmail.com>
2024-01-21 15:45:25 -0800

    task 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,42 +1,36 @@
 export default class HolbertonCourse {
-    constructor(name = '', length = 0, students = []) {
-      this.name = name;
-      this.length = length;
-      this.students = students;
+  constructor(name = '', length = 0, students = []) {
+    this.name = name;
+        this.length = length;
+        this.students = students;
     }
-
-    set name(Name) {
-      if (typeof Name !== 'string') {
-        throw TypeError('Name must be a string');
-      }
-      this._name = Name;
+      set name(Name) {
+        if (typeof Name !== 'string') {
+            throw TypeError('Name must be a string');
+        }
+        this._name = Name;
     }
-
     set length(Length) {
-      if (typeof Length !== 'number') {
-        throw TypeError('Length must be a number');
-      }
-      this._length = Length;
+        if (typeof Length !== 'number') {
+            throw TypeError('Length must be a number');
+        }
+        this._length = Length;
     }
-
     set students(Students) {
-      if (Array.isArray(Students)
+        if (Array.isArray(Students)
             && Students.every((student) => (typeof student === 'string') === true)) {
-        this._students = Students;
-      } else {
-        throw TypeError('Students must be an array of strings');
-      }
+            this._students = Students;
+        } else {
+            throw TypeError('Students must be an array of strings');
+        }
     }
-
     get name() {
-      return this._name;
+        return this._name;
     }
-  
     get length() {
-      return this._length;
+        return this._length;
     }
-
     get students() {
-      return this._students;
+        return this._students;
     }
-  }
+}
