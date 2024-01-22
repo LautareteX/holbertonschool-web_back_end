b86fee59ce8ee13080a2f037a8a8ece6db2c2143
LautareteX <lautarorodriguezcoitinho@gmail.com>
2024-01-21 16:06:07 -0800

    style - rewrite

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -4,18 +4,21 @@ export default class HolbertonCourse {
     this.length = length;
     this.students = students;
   }
+
   set name(Name) {
     if (typeof Name !== 'string') {
       throw TypeError('Name must be a string');
     }
     this._name = Name;
   }
+
   set length(Length) {
     if (typeof Length !== 'number') {
       throw TypeError('Length must be a number');
     }
      this._length = Length;
    }
+
   set students(Students) {
     if (Array.isArray(Students)
             && Students.every((student) => (typeof student === 'string') === true)) {
@@ -24,12 +27,15 @@ export default class HolbertonCourse {
       throw TypeError('Students must be an array of strings');
     }
   }
+
   get name() {
     return this._name;
   }
+
   get length() {
     return this._length;
   }
+
   get students() {
     return this._students;
   }
