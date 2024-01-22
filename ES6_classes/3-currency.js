8df4d7c6f6002278b202e7fb243348db62dd3073
LautareteX <lautarorodriguezcoitinho@gmail.com>
2024-01-21 16:13:56 -0800

    identation

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,33 +1,32 @@
 export default class Currency {
-    constructor(code = '', name = '') {
-      this.code = code;
-      this.name = name;
-    }
-
-    displayFullCurrency() {
-      return `${this.name} (${this.code})`;
-    }
-
-    set code(Code) {
-      if (typeof Code !== 'string') {
-        throw TypeError('Code must by a String');
-      }
-      this._code = Code
-    }
-
-    set name(Name) {
-      if (typeof Name !== 'string') {
-        throw TypeError('Name must by a String');
-      }
-      this._name = Name;
-    }
-
-    get code() {
-      return this._code;
-    }
-
-    get name() {
-
-      return this._name;
-    }
+  constructor(code = '', name = '') {
+    this.code = code;
+    this.name = name;
   }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+
+  set code(Code) {
+    if (typeof Code !== 'string') {
+      throw TypeError('Code must by a String');
+    }
+    this._code = Code
+  }
+
+  set name(Name) {
+    if (typeof Name !== 'string') {
+      throw TypeError('Name must by a String');
+    }
+    this._name = Name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+}
