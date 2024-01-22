1281a4d7da99a9455cff310453cf927c39e906d9
LautareteX <lautarorodriguezcoitinho@gmail.com>
2024-01-21 16:36:35 -0800

    100% correct

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,20 +1,20 @@
 export default class Building {
-    constructor(sqft) {
-      this._sqft = sqft;
-      if (this.constructor !== Building && !this.evacuationWarningMessage) {
-        throw Error('Class extending Building must override evacuationWarningMessage');
-      }
-    }
-
-    get sqft() {
-      return this._sqft;
-    }
-
-    set sqft(Sqft) {
-      if (typeof Sqft === 'number') {
-        this._sqft = Sqft;
-      } else {
-        throw TypeError('Sqft must by a number');
-      }
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(Sqft) {
+    if (typeof Sqft === 'number') {
+      this._sqft = Sqft;
+    } else {
+      throw TypeError('Sqft must by a number');
+    }
+  }
+}
--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,28 +1,28 @@
 export default class Airport {
-    constructor(name, code) {
-      this.name = name;
-      this.code = code;
-    }
-
-    set name(val) {
-      if (typeof val !== 'string') throw TypeError('Name must by a string');
-      this._name = val;
-    }
-
-    set code(val) {
-      if (typeof val !== 'string') throw TypeError('Code must by a string');
-      this._code = val;
-    }
-
-    get name() {
-      return this._name;
-    }
-
-    get code() {
-      return this._code;
-    }
-
-    toString() {
-      return `[object ${this.code}]`;
-    }
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
   }
+
+  set name(val) {
+    if (typeof val !== 'string') throw TypeError('Name must by a string');
+    this._name = val;
+  }
+
+  set code(val) {
+    if (typeof val !== 'string') throw TypeError('Code must by a string');
+    this._code = val;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return `[object ${this.code}]`;
+  }
+}
